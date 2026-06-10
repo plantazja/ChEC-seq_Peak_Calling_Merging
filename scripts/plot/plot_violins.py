38075cbf20da907ded677412fdfd36d593e6a8ba
Anastasia R <a.ramanchanka@student.uw.edu.pl>
2026-06-10 15:02:42 -0500

    Add wig2csv

---
+++ scripts/plot/plot_violins.py
@@ -0,0 +1,44 @@
+import sys, os
+import argparse
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+def cmdline_args():
+    p = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    
+    p.add_argument("--csvs", nargs="+",
+                   help="List of csv files")
+    p.add_argument("--output", type=str,
+                   help="Output png file")
+    return(p.parse_args())
+
+def plot_peaks(samples, output):
+    # Get peaks count
+    samples_dict = defaultdict()
+    for s in samples:
+        df = pd.read_csv(s, sep='\t', header=None)
+        s_name = os.path.basename(s)
+        s_name = s_name.split(".")[0]
+        samples_dict[s_name] = df.sum(axis=0)
+    
+    # Plot violins
+    ax = sns.violinplot(data=samples_dict)
+    ax.set(
+        xlabel="",
+        ylabel="Signal"
+    )
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig(output, dpi=300, bbox_inches='tight')
+    plt.close()
+
+if __name__ == '__main__':
+    try:
+        args = cmdline_args()
+        plot_peaks(args.csvs, args.output)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
